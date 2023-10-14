4817a9c5210f1dfab6754355e8f49b4497d173b5
Shaheem Shedid <shaheemshedid4@gmail.com>
2023-10-14 10:47:23 -0400

    pageWeb

---
+++ web/geekfest/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="login">
+          <box>
+        Login <input/>
+        Password <input/>
+          </box>
+      </div>
+    </div>
+  );
+
+}
+
+export default App;
